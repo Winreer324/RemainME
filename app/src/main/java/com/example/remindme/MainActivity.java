0ed80ea2fd114e53ed1413d78e26b6b60a7d0b1b
Winreer324 <32igor3@mail.ru>
2018-11-12 17:09:15 +0300

    commit

--- app/src/main/java/com/example/remindme/MainActivity.java
+++ app/src/main/java/com/example/remindme/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.remindme;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+//        setSupportActionBar(toolbar);
+    }
+
+    private void initToolbar() {
+            toolbar =  findViewById(R.id.toolbar);
+            toolbar.setTitle(R.string.app_name);
+            toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    return false;
+                }
+            });
+            toolbar.inflateMenu(R.menu.menu);
+//            setSupportActionBar(toolbar);
     }
 }
